9e2748c5635f242e2e1834e86b100521393005b7
Richard <samedii@gmail.com>
2020-03-04 19:40:59 +0100

    Update title

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Psyched</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Let's recreate Psyched in react!
         </p>
       </div>
     );
